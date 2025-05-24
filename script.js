cad690676a68b79431bfb27e7d505259be91c40c
Maxim Byko-Ianko <maxim@MacBookPro.lan>
2025-05-24 14:24:19 +0300

    basis script

--- script.js
+++ script.js
@@ -3,17 +3,31 @@ add = document.querySelector('#add');
 /** @type {HTMLDivElement} */
 toDo = document.querySelector('.to-do');
 
-
-add.addEventListener('click', ()=>{
-  let problem = prompt('Введите задачу', '');
-  let description = prompt('Введите описание', '');
-  let htmlCart = `<div class="cart">
+add.addEventListener('click', () => {
+	let problem = prompt('Введите задачу', '');
+	let description = prompt('Введите описание', '');
+	let htmlCart = `<div class="cart">
     <h1>${problem}</h1>
     <p>${description}</p>
-    <button>Удалить</button>
-    <button>Редактировать</button>
+    <button id="del">Удалить</button>
+    <button id="red">Редактировать</button>
    </div>`;
-  let div = document.createElement('div');
-  div.innerHTML = htmlCart;
-  toDo.appendChild(div);
+	let div = document.createElement('div');
+	div.innerHTML = htmlCart;
+	div.classList.toggle('style-carts', true);
+	toDo.appendChild(div);
+
+	const del = div.querySelector('#del');
+	del.addEventListener('click', () => {
+		toDo.removeChild(div);
+	});
+  const red = div.querySelector('#red');
+	red.addEventListener('click', () => {
+    const problem = prompt('Введите задачу', '');
+		const description = prompt('Введите описание', '');
+    const h1 = div.querySelector('h1');
+    h1.textContent = problem;
+    const p = div.querySelector('p');
+    p.textContent = description
+	});
 });
